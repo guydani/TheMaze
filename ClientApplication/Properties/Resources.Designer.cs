f3e28e4f08d361085deee9555f8afa703fab0421
Guy Dani <guydani@gmail.com>
2018-03-05 21:21:21 +0200

    ...?

--- ClientApplication/Properties/Resources.Designer.cs
+++ ClientApplication/Properties/Resources.Designer.cs
@@ -69,5 +69,15 @@ namespace ClientApplication.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap vectorstash_pacman {
+            get {
+                object obj = ResourceManager.GetObject("vectorstash_pacman", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
